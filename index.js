fb9cd5c08a05cb8b5ff80a55fc989fafffbd626a
adarvin14 <amdarvin14@gmail.com>
2021-02-28 16:45:16 -0500

    commit tree 9a6da06355ffb41b44136e848b434d43beea0413

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const testVar = {}
+
+function testFunc() {
+  return "hi"
+}
+
+function superbowlWin(record){
+  let win = record.find(function(w) { return w.result === "W" })
+  if (win) {
+    return win.year
+  } else {
+    return win
+  }
+}
